94511b6be07f0afc5a8f32184282b7628aaaff86
vlad_dumitru <vlad_dumitru@yahoo.com>
2020-07-14 07:21:10 +0300

    FizzBuzz

---
+++ src.main.java/com/vlad/algorithms/FizzBuzz.java
@@ -0,0 +1,20 @@
+package com.vlad.algorithms;
+
+import java.util.stream.IntStream;
+
+/*
+Write a short program that prints each number from 1 to 100 on a new line.
+
+For each multiple of 3, print "Fizz" instead of the number.
+
+For each multiple of 5, print "Buzz" instead of the number.
+
+For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+ */
+public class FizzBuzz {
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 100)
+                .mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i))
+                .forEach(System.out::println);
+    }
+}
